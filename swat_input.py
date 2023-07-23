a9fb7d387770e0f7c1ab94688cef4b69c16a949c
akhi9661 <akhiraj9661@gmail.com>
2023-07-24 00:29:14 +0530

    Added swat_input.py

---
+++ swat_input.py
@@ -0,0 +1,393 @@
+import tkinter as tk
+from tkinter import ttk, scrolledtext, filedialog
+import tkintermapview
+from tkcalendar import DateEntry
+from datetime import datetime
+
+import os
+import json
+import requests
+import pandas as pd
+
+def create_regular_grid(min_lat, min_lon, max_lat, max_lon, step=0.5):
+    grid_points = []
+    lat = max_lat
+    while lat >= min_lat:
+        lon = min_lon
+        while lon <= max_lon:
+            grid_points.append((lat, lon))
+            lon += step
+        lat -= step
+    return grid_points
+
+def download_param(bounding_box, param = 'T2M', community = 'AG', temporal = 'daily', 
+                   start_date = '20150101', end_date = '20150331', dest_folder = os.getcwd()):
+    
+    # Ref: https://power.larc.nasa.gov/#resources
+    # param: ['T2M_MAX', 'T2M_MIN', 'RH2M', 'PRECTOTCORR', 'WS2M', 'ALLSKY_SFC_SW_DWN']
+    # community: AG, RE, SB 
+    # date: YYYYMMDD
+    # 
+    
+    opf = os.path.join(dest_folder, f'{param}')
+    os.makedirs(opf, exist_ok = True)
+    
+    locations = create_regular_grid(*bounding_box)
+    n = 1
+    
+    # Initialize the index data
+    index_data = [['ID', 'NAME', 'LAT', 'LONG', 'ELEVATION']]
+    idx = 1
+
+    if param == 'T2M':
+        # Download T2M_MAX data
+        data_dict_max = {}
+        for latitude, longitude in locations:
+            url = f'https://power.larc.nasa.gov/api/temporal/{temporal}/point?'
+            url += f'parameters=T2M_MAX&community={community}&longitude={longitude}&latitude={latitude}&start={start_date}&end={end_date}&format=JSON'
+            print(f'Downloading "T2M_MAX" [{(latitude, longitude)}]: {(n/len(locations))*100:.2f}%', end='\r')
+            response = requests.get(url=url, verify=True, timeout=30.00)
+            content = json.loads(response.content.decode('utf-8'))
+            properties = content['properties']['parameter']['T2M_MAX']
+            data_dict_max[(latitude, longitude)] = properties
+            
+            # Prepare data for the index file
+            filename = f"T2M{latitude}{longitude}".replace(".", "")
+            elevation = -999.000  # Use -999.000 as a constant value for elevation
+            index_data.append([idx, filename, latitude, longitude, elevation])
+            idx += 1
+            
+            n += 1
+
+        df_max = pd.DataFrame.from_dict(data_dict_max, orient='index')
+        df_max = df_max.transpose()
+
+        # Download T2M_MIN data
+        data_dict_min = {}
+        n = 1
+        for latitude, longitude in locations:
+            url = f'https://power.larc.nasa.gov/api/temporal/daily/point?'
+            url += f'parameters=T2M_MIN&community={community}&longitude={longitude}&latitude={latitude}&start={start_date}&end={end_date}&format=JSON'
+            print(f'Downloading "T2M_MIN" [{(latitude, longitude)}]: {(n/len(locations))*100:.2f}%', end='\r')
+            response = requests.get(url=url, verify=True, timeout=30.00)
+            content = json.loads(response.content.decode('utf-8'))
+            properties = content['properties']['parameter']['T2M_MIN']
+            data_dict_min[(latitude, longitude)] = properties
+            n += 1
+
+        df_min = pd.DataFrame.from_dict(data_dict_min, orient='index')
+        df_min = df_min.transpose()
+        
+        # Combine the values in df_max and df_min into a single column with comma-separated values
+        combined_df = df_max.astype(str) + ',' + df_min.astype(str)
+        combined_df.columns = [f"T2M{lat}{lon}".replace(".", "") for lat, lon in combined_df.columns]
+        date_row = pd.DataFrame({col: [start_date] for col in combined_df.columns})
+        combined_df = pd.concat([date_row, combined_df], ignore_index=True)
+        df = combined_df
+        for column in df.columns:
+            file_name = f"{column}.txt"
+            file_path = os.path.join(opf, file_name)
+            df[column].to_csv(file_path, header=False, index=False, sep='\t')
+            
+        # Save index data to a separate CSV file
+        index_df = pd.DataFrame(index_data[1:], columns=index_data[0])
+        index_df.to_csv(os.path.join(opf, f"{param}.txt"), index=False)
+    
+    else:
+        data_dict = {}
+        for latitude, longitude in locations:
+            url = f'https://power.larc.nasa.gov/api/temporal/daily/point?'
+            url += f'parameters={param}&community={community}&longitude={longitude}&latitude={latitude}&start={start_date}&end={end_date}&format=JSON'
+            print(f'Downloading [{(latitude, longitude)}]: {(n/len(locations))*100:.2f}%', end='\r')
+            response = requests.get(url=url, verify=True, timeout=30.00)
+            content = json.loads(response.content.decode('utf-8'))
+            properties = content['properties']['parameter'][param]
+            data_dict[(latitude, longitude)] = properties
+            
+            # Prepare data for the index file
+            filename = f"{param}{str(latitude)}{str(longitude)}".replace(".", "")
+            elevation = -999.000  # Use -999.000 as a constant value for elevation
+            index_data.append([idx, filename, latitude, longitude, elevation])
+            idx += 1
+            
+            n += 1
+            
+        df = pd.DataFrame.from_dict(data_dict, orient='index')
+        df = df.transpose()
+        df.columns = [f"{param}{str(lat)}{str(lon)}".replace(".", "") for lat, lon in locations]
+
+        # Insert start date in the first row
+        date_row = pd.DataFrame(columns = df.columns)
+        date_row.loc[0] = start_date
+        df = pd.concat([date_row, df]).reset_index(drop = True)
+
+        for column in df.columns:
+            file_name = f"{column}.txt"
+            file_path = os.path.join(opf, file_name)
+            df[column].to_csv(file_path, header = False, index = False)
+            
+        # Save index data to a separate CSV file
+        index_df = pd.DataFrame(index_data[1:], columns=index_data[0])
+        index_df.to_csv(os.path.join(opf, f"{param}.txt"), index=False)
+        
+    return df
+    
+def browse_dest_ee_folder():
+    dest_folder = filedialog.askdirectory()
+    dest_fol_entry.delete(0, tk.END)
+    dest_fol_entry.insert(0, dest_folder)
+
+def on_menu_selected(event, event_type):
+    selected_task = event.widget.get()
+    event_type_messages = {
+        "param": "Parameter",
+        "com": "Community",
+        # Add more date type messages as needed
+    }
+
+    if event_type in event_type_messages:
+        event_label = event_type_messages[event_type]
+        print(f"{event_label} selected: {selected_task}")
+
+        status_label.insert(tk.END, f'{event_label}: {selected_task}\n')
+        status_label.see(tk.END)
+        status_label.update()
+
+main_window = tk.Tk()
+main_window.title("SWAT Weather Data Downloader")
+main_window.geometry("800x500+10+10")
+
+# Task combobox
+task_list = ["Select Parameter",
+             "Precipitation",
+             "Temperature",
+             "Wind",
+             "Solar Radiation",
+             "Relative Humidity",
+             "About"]
+    
+# Function to handle date selection
+def on_date_selected(event, date_type):
+    if date_type == "temporal":
+        selected_date = event.widget.get()
+    else:
+        selected_date = event.widget.get_date()
+    
+    date_type_messages = {
+        "start": "Start Date",
+        "end": "End Date",
+        "temporal": "Temporal Scale",
+        # Add more date type messages as needed
+    }
+
+    if date_type in date_type_messages:
+        date_label = date_type_messages[date_type]
+        print(f"{date_label} selected: {selected_date}")
+
+        status_label.insert(tk.END, f'{date_label}: {selected_date}\n')
+        status_label.see(tk.END)
+        status_label.update()
+    
+def polygon_click(polygon):
+    print(f"polygon clicked - text: {polygon.name}")
+    
+def convert_date_format(date_str):
+    date_obj = datetime.strptime(date_str, '%d-%m-%Y')
+    formatted_date = date_obj.strftime('%Y%m%d')
+    return formatted_date
+    
+# Function to handle fetching power data and creating a bounding box
+def fetch_power_data():
+    try:
+        # Get the entered values for latitude and longitude
+        bottom_left_lat = float(bottom_left_lat_entry.get())
+        bottom_left_lon = float(bottom_left_lon_entry.get())
+        upper_right_lat = float(upper_right_lat_entry.get())
+        upper_right_lon = float(upper_right_lon_entry.get())
+
+        bbox = [bottom_left_lat, bottom_left_lon, upper_right_lat, upper_right_lon]
+
+        # Create the bounding box list with tuples
+        bounding_box = [
+            (upper_right_lat, bottom_left_lon),  # Top-Left
+            (upper_right_lat, upper_right_lon),  # Top-Right
+            (bottom_left_lat, upper_right_lon),  # Bottom-Right
+            (bottom_left_lat, bottom_left_lon)   # Bottom-Left
+        ]
+        
+        status_label.insert(tk.END, f'Bounding Box: {bounding_box}')
+        status_label.see(tk.END)
+        status_label.update()
+        
+        polygon = map_widget.set_polygon(bounding_box, fill_color=None, outline_color="red", border_width=2,
+                                         command=polygon_click,
+                                         name="Bounding Box")
+        
+        # first coordinate is the top-left corner and the second coordinate is the bottom-right corner
+        map_widget.fit_bounding_box(bounding_box[0], bounding_box[2])
+        
+        # Getting other variables
+
+        # Mapping of task_var options to parameters
+        task_to_param = {
+            'Precipitation': 'PRECTOTCORR',
+            'Temperature': 'T2M',
+            'Solar Radiation': 'ALLSKY_SFC_SW_DWN',
+            'Relative Humidity': 'RH2M',
+            'Wind': 'WS2M',}
+
+        # Mapping of com_var options to communities
+        com_to_community = {
+            'Agroclimatology (AG)': 'AG',
+            'Sustainable Buildings (SB)': 'SB',
+            'Renewable Energy (RE)': 'RE',}
+
+        param = task_to_param.get(task_var.get(), None)
+        community = com_to_community.get(com_var.get(), None)
+            
+        temporal = time_var.get()
+        dest_folder = dest_fol_entry.get()
+        
+        start_date = convert_date_format(date_str = start_date_entry.get())
+        end_date = convert_date_format(date_str = end_date_entry.get())
+        
+        df = download_param(bbox, param, community, temporal, start_date, end_date, dest_folder)
+
+    except ValueError:
+        print("Invalid input. Please enter valid numeric values for latitude and longitude.")
+
+# Create a frame for the inputs
+input_frame = ttk.LabelFrame(main_window, text="Data Inputs")
+input_frame.grid(row = 0, sticky = 'news', padx=10, pady=10)
+
+# Create a frame for the "1. Select Parameter/Community" section
+param_frame = ttk.LabelFrame(input_frame, text="1. Select Parameter and Community", borderwidth=2, relief=tk.RIDGE)
+param_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
+
+task_var = tk.StringVar(param_frame)
+task_var.set("Select Parameter")
+task_combobox = ttk.Combobox(param_frame, textvariable=task_var, values=task_list)
+task_combobox.grid(row=0, column=0, sticky = 'news', padx=5, pady=5)
+task_combobox.bind("<<ComboboxSelected>>", lambda event: on_menu_selected(event, "param"))
+
+com_var = tk.StringVar(param_frame)
+com_var.set("Agroclimatology (AG)")
+com_combobox = ttk.Combobox(param_frame, textvariable=com_var, values=['Agroclimatology (AG)', 'Sustainable Buildings (SB)',
+                                                                        'Renewable Energy (RE)'])
+com_combobox.grid(row=0, column=1, sticky = 'news', padx=5, pady=5)
+com_combobox.bind("<<ComboboxSelected>>", lambda event: on_menu_selected(event, "com"))
+
+# Add a separator between Parameter and "2. Enter Date Range" sections
+separator = ttk.Separator(input_frame, orient=tk.HORIZONTAL)
+separator.grid(row=1, column=0, columnspan=2, pady=10, sticky='ew')
+
+# Create a frame for the "2. Enter Date Range" section
+date_frame = ttk.LabelFrame(input_frame, text="2. Date Range and Temporal Scale")
+date_frame.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
+
+# Start Date Input with Calendar
+start_date_label = ttk.Label(date_frame, text="Start Date")
+start_date_label.grid(row=0, column=0, padx=5, pady=5, sticky='news')
+start_date_entry = DateEntry(date_frame, date_pattern='dd-mm-yyyy')
+start_date_entry.grid(row=0, column=1, padx=5, pady=5, sticky='news')
+start_date_unit_label = ttk.Label(date_frame, text=" (DD-MM-YYYY)")
+start_date_unit_label.grid(row=0, column=2, padx=5, pady=5, sticky='news')
+start_date_entry.bind("<<DateEntrySelected>>", lambda event: on_date_selected(event, "start"))
+
+# End Date Input with Calendar
+end_date_label = ttk.Label(date_frame, text="End Date")
+end_date_label.grid(row=1, column=0, padx=5, pady=5, sticky='news')
+end_date_entry = DateEntry(date_frame, date_pattern='dd-mm-yyyy')
+end_date_entry.grid(row=1, column=1, padx=5, pady=5, sticky='news')
+end_date_unit_label = ttk.Label(date_frame, text=" (DD-MM-YYYY)")
+end_date_unit_label.grid(row=1, column=2, padx=5, pady=5, sticky='news')
+end_date_entry.bind("<<DateEntrySelected>>", lambda event: on_date_selected(event, "end"))
+
+# Temporal Scale Combobox
+time_label = ttk.Label(date_frame, text='Temporal Scale')
+time_label.grid(row=2, column=0, padx=5, pady=5, sticky='news')
+time_var = tk.StringVar(date_frame)
+time_var.set("Daily")
+time_combobox = ttk.Combobox(date_frame, textvariable=time_var, values=['Daily', 'Monthly', 'Yearly'])
+time_combobox.grid(row=2, column=1, sticky='news', padx=5, pady=5)
+time_combobox.bind("<<ComboboxSelected>>", lambda event: on_date_selected(event, "temporal"))
+
+# Corner coordinates 
+# Add a separator between End Date and "3. Enter Lat/Lon" sections
+separator = ttk.Separator(input_frame, orient=tk.HORIZONTAL)
+separator.grid(row=3, column=0, columnspan=2, pady=10, sticky='ew')
+
+# Create a frame for the "3. Enter Lat/Lon" section
+lat_lon_frame = ttk.LabelFrame(input_frame, text="3. Enter Lat/Lon")
+lat_lon_frame.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
+
+# Bottom-left Lat Input
+bottom_left_lat_label = ttk.Label(lat_lon_frame, text="Bottom-left Lat:")
+bottom_left_lat_label.grid(row=0, column=0, padx=5, pady=5, sticky = 'news')
+bottom_left_lat_entry = ttk.Entry(lat_lon_frame)
+bottom_left_lat_entry.grid(row=0, column=1, padx=5, pady=5, sticky = 'news')
+bottom_left_lat_unit_label = ttk.Label(lat_lon_frame, text=" (Decimal Degrees)")
+bottom_left_lat_unit_label.grid(row=0, column=2, padx=5, pady=5, sticky = 'news')
+
+# Bottom-left Lon Input
+bottom_left_lon_label = ttk.Label(lat_lon_frame, text="Bottom-left Lon:")
+bottom_left_lon_label.grid(row=1, column=0, padx=5, pady=5, sticky = 'news')
+bottom_left_lon_entry = ttk.Entry(lat_lon_frame)
+bottom_left_lon_entry.grid(row=1, column=1, padx=5, pady=5, sticky = 'news')
+bottom_left_lon_unit_label = ttk.Label(lat_lon_frame, text=" (Decimal Degrees)")
+bottom_left_lon_unit_label.grid(row=1, column=2, padx=5, pady=5, sticky = 'news')
+
+# Upper-right Lat Input
+upper_right_lat_label = ttk.Label(lat_lon_frame, text="Upper-right Lat:")
+upper_right_lat_label.grid(row=2, column=0, padx=5, pady=5, sticky = 'news')
+upper_right_lat_entry = ttk.Entry(lat_lon_frame)
+upper_right_lat_entry.grid(row=2, column=1, padx=5, pady=5, sticky = 'news')
+upper_right_lat_unit_label = ttk.Label(lat_lon_frame, text=" (Decimal Degrees)")
+upper_right_lat_unit_label.grid(row=2, column=2, padx=5, pady=5, sticky = 'news')
+
+# Upper-right Lon Input
+upper_right_lon_label = ttk.Label(lat_lon_frame, text="Upper-right Lon:")
+upper_right_lon_label.grid(row=3, column=0, padx=5, pady=5, sticky = 'news')
+upper_right_lon_entry = ttk.Entry(lat_lon_frame)
+upper_right_lon_entry.grid(row=3, column=1, padx=5, pady=5, sticky = 'news')
+upper_right_lon_unit_label = ttk.Label(lat_lon_frame, text=" (Decimal Degrees)")
+upper_right_lon_unit_label.grid(row=3, column=2, padx=5, pady=5, sticky = 'news')
+
+separator = ttk.Separator(input_frame, orient=tk.HORIZONTAL)
+separator.grid(row=5, column=0, columnspan=2, pady=10, sticky='ew')
+
+opf_frame = ttk.LabelFrame(input_frame, text = '4. Output Folder')
+opf_frame.grid(row = 6, column = 0, columnspan = 2, padx = 10, pady = 10, sticky = 'ew')
+
+dest_fol_button = ttk.Button(opf_frame, text="Browse Destination Folder", command=browse_dest_ee_folder)
+dest_fol_button.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
+dest_fol_entry = ttk.Entry(opf_frame)
+dest_fol_entry.grid(row=0, column=1, columnspan = 2, padx=5, pady=5, sticky="news")
+
+separator = ttk.Separator(input_frame, orient=tk.HORIZONTAL)
+separator.grid(row=7, column=0, columnspan=2, pady=10, sticky='ew')
+
+# Fetch Data Button
+fetch_button = ttk.Button(input_frame, text="Fetch Data", command=fetch_power_data)
+fetch_button.grid(row=8, column=0, columnspan=2, padx=5, pady=10, sticky = 'news')
+
+# -------------- Map -----------
+map_label = ttk.LabelFrame(main_window)
+map_label.grid(row = 0, column = 1, sticky = 'news', padx = 10, pady = 10)
+
+map_widget = tkintermapview.TkinterMapView(map_label, width=800, height=600, corner_radius=0)
+map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+map_widget.grid(row = 0, columnspan = 2, sticky = 'news')
+map_widget.set_position(20.5937, 78.9629)  
+map_widget.set_zoom(4.5)
+
+# ------ Console ------ 
+separator = ttk.Separator(map_label, orient=tk.HORIZONTAL)
+separator.grid(row=1, column=0, columnspan=2, pady=10, sticky='ew')
+
+status_label = scrolledtext.ScrolledText(map_label, width=80, height=5)
+status_label.grid(row = 2, columnspan = 2, sticky = 'news')
+
+main_window.mainloop()
+
+# ---------------------------------------------------------------------------------------------- #
